41e847d4cb22a5fdffbf4599066d82d02e37893b
David Mann <mannd@epstudiossoftware.com>
2017-09-02 16:12:26 -0600

    First commit

---
+++ CommonSource/QTc.h
@@ -0,0 +1,19 @@
+//
+//  QTc_iOS.h
+//  QTc_iOS
+//
+//  Created by David Mann on 9/2/17.
+//  Copyright © 2017 EP Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QTc_iOS.
+FOUNDATION_EXPORT double QTc_iOSVersionNumber;
+
+//! Project version string for QTc_iOS.
+FOUNDATION_EXPORT const unsigned char QTc_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QTc_iOS/PublicHeader.h>
+
+
---
+++ CommonSource/QTc.swift
@@ -0,0 +1,15 @@
+//
+//  QTc.swift
+//  QTc
+//
+//  Created by David Mann on 9/2/17.
+//  Copyright © 2017 EP Studios. All rights reserved.
+//
+
+import Foundation
+
+@objc public class AddIt : NSObject {
+    public static func add(_ x: Double, _ y: Double) -> Double {
+        return x + y
+    }
+}
