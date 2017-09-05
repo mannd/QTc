7b33f821f01d76ac44e918b9dfaef167f3626d49
David Mann <mannd@epstudiossoftware.com>
2017-09-04 21:45:05 -0600

    Add QtcBZT

--- CommonSource/QTc.swift
+++ CommonSource/QTc.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/// TODO: is @objc tag needed if inheritance from NSObject?
 @objc public class QTc : NSObject {
     // static conversion functions
     static func secToMsec(_ sec: Double) -> Double {
@@ -32,6 +33,25 @@ import Foundation
 
     static func msecToBpm(_ msec: Double) -> Double {
         return 60000 / msec
+    }
+    
+    // QTc formulae
+    
+    // Bazett (QTcBZT)
+    static func qtcBzt(qtInSec: Double, rrInSec: Double) -> Double {
+        return qtInSec / sqrt(rrInSec)
+    }
+    
+    static func qtcBzt(qtInMsec: Double, rrInMsec: Double) -> Double {
+        return secToMsec(qtcBzt(qtInSec:msecToSec(qtInMsec), rrInSec:msecToSec(rrInMsec)))
+    }
+    
+    static func qtcBzt(qtInSec: Double, rate: Double) -> Double {
+        return qtcBzt(qtInSec:qtInSec, rrInSec: bpmToSec(rate))
+    }
+    
+    static func qtcBzt(qtInMsec: Double, rate: Double) -> Double {
+        return qtcBzt(qtInMsec: qtInMsec, rrInMsec: bpmToMsec(rate))
     }
 }
 
--- CommonTests/QTc_iOSTests.swift
+++ CommonTests/QTc_iOSTests.swift
@@ -17,95 +17,19 @@ class QTc_iOSTests: XCTestCase {
     let veryVeryRoughDelta = 1.0 // accurate to 1 integer
     // source: https://link.springer.com/content/pdf/bfm%3A978-3-642-58810-5%2F1.pdf
     // Note this table is not accurate within 0.5 bpm when converting back from interval to rate
-    let rateIntervalTable: [(rate: Double, interval: Double)] = [(20, 3000),
-                                                         (25, 2400),
-                                                         (30, 2000),
-                                                         (35, 1714),
-                                                         (40, 1500),
-                                                         (45, 1333),
-                                                         (50, 1200),
-                                                         (55, 1091),
-                                                         (60, 1000),
-                                                         (65, 923),
-                                                         (70, 857),
-                                                         (75, 800),
-                                                         (80, 750),
-                                                         (85, 706),
-                                                         (90, 667),
-                                                         (95, 632),
-                                                         (100, 600),
-                                                         (105, 571),
-                                                         (110, 545),
-                                                         (115, 522),
-                                                         (120, 500),
-                                                         (125, 480),
-                                                         (130, 462),
-                                                         (135, 444),
-                                                         (140, 429),
-                                                         (145, 414),
-                                                         (150, 400),
-                                                         (155, 387),
-                                                         (160, 375),
-                                                         (165, 364),
-                                                         (170, 353),
-                                                         (175, 343),
-                                                         (180, 333),
-                                                         (185, 324),
-                                                         (190, 316),
-                                                         (195, 308),
-                                                         (200, 300),
-                                                         (205, 293),
-                                                         (210, 286),
-                                                         (215, 279),
-                                                         (220, 273),
-                                                         (225, 267),
-                                                         (230, 261),
-                                                         (235, 255),
-                                                         (240, 250),
-                                                         (245, 245),
-                                                         (250, 240),
-                                                         (255, 235),
-                                                         (260, 231),
-                                                         (265, 226),
-                                                         (270, 222),
-                                                         (275, 218),
-                                                         (280, 214),
-                                                         (285, 211),
-                                                         (290, 207),
-                                                         (295, 203),
-                                                         (300, 200),
-                                                         (305, 197),
-                                                         (310, 194),
-                                                         (315, 190),
-                                                         (320, 188),
-                                                         (325, 185),
-                                                         (330, 182),
-                                                         (335, 179),
-                                                         (340, 176),
-                                                         (345, 174),
-                                                         (350, 171),
-                                                         (355, 169),
-                                                         (360, 167),
-                                                         (365, 164),
-                                                         (370, 162),
-                                                         (375, 160),
-                                                         (380, 158),
-                                                         (385, 156),
-                                                         (390, 154),
-                                                         (395, 152),
-                                                         (400, 150)]
+    let rateIntervalTable: [(rate: Double, interval: Double)] = [(20, 3000),  (25, 2400), (30, 2000), (35, 1714), (40, 1500), (45, 1333), (50, 1200), (55, 1091), (60, 1000), (65, 923), (70, 857), (75, 800), (80, 750), (85, 706), (90, 667), (95, 632), (100, 600), (105, 571), (110, 545), (115, 522), (120, 500), (125, 480), (130, 462), (135, 444), (140, 429), (145, 414), (150, 400), (155, 387), (160, 375), (165, 364), (170, 353), (175, 343), (180, 333), (185, 324), (190, 316), (195, 308), (200, 300), (205, 293), (210, 286), (215, 279), (220, 273), (225, 267), (230, 261), (235, 255), (240, 250), (245, 245), (250, 240), (255, 235), (260, 231), (265, 226), (270, 222), (275, 218), (280, 214), (285, 211), (290, 207), (295, 203), (300, 200), (305, 197), (310, 194), (315, 190), (320, 188), (325, 185), (330, 182), (335, 179), (340, 176), (345, 174), (350, 171), (355, 169), (360, 167), (365, 164), (370, 162), (375, 160), (380, 158), (385, 156), (390, 154), (395, 152), (400, 150)]
 
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
+ 
+ override func setUp() {
+ super.setUp()
+ // Put setup code here. This method is called before the invocation of each test method in the class.
+ }
+ 
+ override func tearDown() {
+ // Put teardown code here. This method is called after the invocation of each test method in the class.
+ super.tearDown()
+ }
+ 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -145,6 +69,14 @@ class QTc_iOSTests: XCTestCase {
             XCTAssertEqualWithAccuracy(QTc.bpmToMsec(rate), interval, accuracy: veryRoughDelta)
             XCTAssertEqualWithAccuracy(QTc.msecToBpm(interval), rate, accuracy: veryVeryRoughDelta)
         }
+    }
+    
+    func testQTcFunctions() {
+        // QTcBZT (Bazett)
+        XCTAssertEqualWithAccuracy(QTc.qtcBzt(qtInSec:0.3, rrInSec:1.0), 0.3, accuracy: delta)
+        XCTAssertEqualWithAccuracy(QTc.qtcBzt(qtInMsec:300, rrInMsec:1000), 300, accuracy:delta)
+        XCTAssertEqualWithAccuracy(QTc.qtcBzt(qtInSec: 0.3, rate: 60), 0.3, accuracy: delta)
+        XCTAssertEqualWithAccuracy(QTc.qtcBzt(qtInMsec: 300, rate: 60), 300, accuracy: delta)
     }
     
 }
